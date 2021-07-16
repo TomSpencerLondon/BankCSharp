b779392a5f58ec52032e39a2acd119ef4b9cc39f
Tom <thomas.spencer@codurance.com>
2021-07-17 00:56:43 +0100

    add starter

---
+++ BankApplication.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace BankApplication.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
---
+++ BankApplication/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace BankApplication
+{
+    public class Class1
+    {
+    }
+}
